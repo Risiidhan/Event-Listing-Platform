06d54375e6867f13f46af52cf2ee44b0bd63972b
Risiidhan <rkishon4@gmail.com>
2025-05-14 22:34:02 +0400

    minor ui changes

--- src/components/page/home/EventComponent.tsx
+++ src/components/page/home/EventComponent.tsx
@@ -6,33 +6,30 @@ import { FaCalendarDays, FaLocationDot } from 'react-icons/fa6'
 
 const EventComponent = ({ event }: { event: any }) => {
     return (
-        <div className="bg-white flex flex-col rounded-xl shadow-md overflow-hidden transition hover:shadow-lg">
+        <div className=" flex flex-col relative rounded-xl  overflow-hidden transition ">
             <FallBackImgComponent
                 width={800}
                 height={800}
                 src={event?.image_url}
                 alt={event?.title}
-                className="w-full h-48 object-cover"
+                className="w-full rounded-2xl h-48 object-cover"
             />
-            <div className="p-4 flex flex-col gap-[6px] flex-1">
-                <h2 className="text-xl font-bold">{event?.title}</h2>
-                <p className="text-sm flex gap-1 items-center text-gray-600">
-                   <FaLocationDot/>
-                    {event?.location}
-                </p>
-                <p className="text-sm flex gap-1 items-center text-gray-600">
-                    <FaCalendarDays />
-                    {new Date(event?.starts_at).toLocaleDateString()} -{" "}
+            <div className="p-[12px] flex flex-col gap-[6px] flex-1">
+                <h2 className="text-[18px] font-semibold text-gray-600">{event?.title}</h2>
+                <p className="text-[14px] flex gap-1 items-center text-gray-600">
+                    <FaLocationDot />
+                    {event?.location} | {new Date(event?.starts_at).toLocaleDateString()} -{" "}
                     {new Date(event?.expires_at).toLocaleDateString()}
                 </p>
 
-                <p className="text-gray-700 flex-1">{event?.description}</p>
+                <p className="text-gray-700 text-[14px] flex-1">{event?.description}</p>
                 <div className="text-gray-700"><TimerComponent event={event} /></div>
-                <p className="text-sm text-gray-700 border-1 rounded-full w-fit px-2 py-[2px] font-medium">
+                <div className="text-sm absolute top-[12px] left-[12px] bg-white text-gray-700 border-1 rounded-full w-fit px-2 py-[2px] font-medium">
                     {event?.type}
-                </p>
+                </div>
             </div>
         </div>
+
     )
 }
 
